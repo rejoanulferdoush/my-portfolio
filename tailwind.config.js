d41929fb12b4fabc7be6dcb9f7ad09a7531f821b
rejoanulferdoushadn <rejoanulferdoushadn@gmail.com>
2024-08-23 21:27:21 +0600

    New changes done in config js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,3 +18,25 @@ module.exports = {
     ],
   },
 };
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          primary: "#469bea",
+          "primary-focus": "#328ce0",
+        },
+      },
+    ],
+  },
+};
+
+// hello another changes
